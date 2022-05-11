ccd8c6d79fc0d3c9cd37c47412aeb4ff18c1ddc0
周子正 <zeroclemontea@163.com>
2022-05-11 22:51:42 +0800

    搭建接口调试组件

--- src/plugins/antd.js
+++ src/plugins/antd.js
@@ -2,9 +2,26 @@
 // 手动删除没用用过的组件
 import Vue from 'vue';
 import 'ant-design-vue/lib/menu/style/css';
+import 'ant-design-vue/lib/table/style/css';
+import 'ant-design-vue/lib/input/style/css';
+import 'ant-design-vue/lib/icon/style/css';
+import 'ant-design-vue/lib/button/style/css';
+import 'ant-design-vue/lib/spin/style/css';
 import {
-  Menu
+  Menu,
+  Table,
+  Input,
+  Icon,
+  Button,
+  Spin,
 } from 'ant-design-vue';
+import JsonViewer from 'vue-json-viewer';
 
 /* v1.1.3+ registration methods */
 Vue.use(Menu);
+Vue.use(Table);
+Vue.use(Input);
+Vue.use(Icon);
+Vue.use(Button);
+Vue.use(Spin);
+Vue.use(JsonViewer);
